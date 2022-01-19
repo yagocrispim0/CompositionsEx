60494e3878edc12a13009ce5989e0ed4c949cb71
yagocrispim0 <yagocrispimgomes@gmail.com>
2022-01-19 10:59:03 -0300

    added basic user inputs

--- CompositionEx/Program.cs
+++ CompositionEx/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using CompositionEx.Entities;
+using CompositionEx.Entities.Enums;
+
+Console.Write("Enter department's name: ");
+string departmentName = Console.ReadLine();
+Console.WriteLine("Enter worker data:");
+Console.Write("Name: ");
+string name = Console.ReadLine();
+Console.Write("Level (Junior/Midlevel/Senior): ");
+WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+Console.Write("Base salary: ");
+double bSalary = double.Parse(Console.ReadLine());
+
+Department department = new Department(departmentName);   
+
+Worker worker = new Worker(name, level, bSalary, department);
+
+Console.Write("How many contractst to this worker? ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Enter #" + (n + 1) + " contract data:");
+    Console.Write("Date (DD/MM/YYYY): ");
+    DateTime date = DateTime.Parse(Console.ReadLine());
+    Console.Write("Value per hour: ");
+    double value = double.Parse(Console.ReadLine());
+    Console.Write("Duration (hours): ");
+    int duration = int.Parse(Console.ReadLine());
+
+    HourContract newContract = new HourContract(date, value, duration);
+
+    worker.AddContract(newContract);
+}
